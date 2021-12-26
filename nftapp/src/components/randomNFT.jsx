5ed11f04ab3e6ca9e7d7d7b554d08a102a62b7ae
AndresEspinoza1999 <espinoza.andres1999@gmail.com>
2021-12-25 23:42:13 -0500

    added css and names and owner

--- nftapp/src/components/randomNFT.jsx
+++ nftapp/src/components/randomNFT.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'; 
+import axios from 'axios';
 
 export default class randomNFT extends Component {
     constructor(props) {
@@ -7,39 +7,54 @@ export default class randomNFT extends Component {
         this.state = {
             name: "",
             img: "",
-            age: "",
+             ex: "",
+             creator:"",
         }
     }
-   generateRandom(min, max) {
+    generateRandom(min, max) {
         var num = Math.floor(Math.random() * (max - min + 1)) + min;
-        return  num;
+        return num;
     }
-    
 
-    generateNFT =  async () => {
-        
+
+    generateNFT = async () => {
+
 
 
         // fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=1', options)
         //     .then(response => response.json())
         //     .then(response => console.log(response))
         //     .catch(err => console.error(err));
-       var num=  this.generateRandom(1,50)
-       var location = this.generateRandom(1,num)-1;
-       console.log(location)
-               const res = await axios.get('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit='+ num)
-                this.setState({
-                    img: res.data.assets[location].image_url
-                     
-                })
-            }
+        var num = this.generateRandom(1, 50)
+        var location = this.generateRandom(1, num) - 1;
+        console.log(location)
+        const res = await axios.get('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=' + num)
+        console.log(res.data.assets[location])
+        var creator= res.data.assets[location].creator.user.username;
+        console.log(creator)
+        if(creator === null){
+            this.generateNFT();
+                }
+        else{
+            this.setState({
+                img: res.data.assets[location].image_url,
+                name: res.data.assets[location].name,
+                creator: creator,
+            })  
+        }
+        
+    }
     render() {
         return (
             <div>
                 <h2>Random NFT Generator</h2>
                 <button onClick={this.generateNFT}>Generate NFT</button>
-                <div className="nftContainer">
-                   <img src={this.state.img} alt="This NFT Doesnt have a registered URL" />
+                <div className="nftImage">
+                    <img src={this.state.img} alt="This NFT Doesnt have a registered URL" />   
+                </div>
+                <div className="mainInformation">
+                    <h3>{this.state.name} by {this.state.creator}</h3>
+                    
                 </div>
             </div>
         )
