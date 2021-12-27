042085c07b9193ff1952a486968f4c742edf178a
AndresEspinoza1999 <espinoza.andres1999@gmail.com>
2021-12-27 10:20:02 -0500

    added routing

--- nftapp/src/index.js
+++ nftapp/src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import RandomNFT from './components/randomNFT'
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Random from "./components/randomNFT";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RandomNFT /> 
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/OpenSeaApiProject" element={<App />} />
+      <Route path="random" element={<Random />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
